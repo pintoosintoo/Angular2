8869e4358ec9d599b863b6494772238bb73f1588
pintoo <pintoosintoo@gmail.com>
2017-06-04 00:17:07 +0530

    initial commit
    initial commit

---
+++ Ang2Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ang2Demo.Startup))]
+namespace Ang2Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
